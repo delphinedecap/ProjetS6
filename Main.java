9d8ba13bb9bbab4cc80dd7ef6f483d3ca03d61bf
JPX87 <jeremy.patapy@gmail.com>
2023-12-13 16:07:03 +0100

    Add Jeremy

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "Benjamin";
+        name += " | Jérémy";
         System.out.println("Hello World : " + name);
         System.out.println("test push git");
     }
