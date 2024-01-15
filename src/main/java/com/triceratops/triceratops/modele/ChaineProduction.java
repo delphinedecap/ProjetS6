9b3de2a3fc7f8cd78c5062089f87961483dfef14
Tartopoiro <bbasset.benjamin@gmail.com>
2024-01-15 19:12:39 +0100

    ajout classe modele
    ajout des classes chaine de production et produit. ajout de la dependance jackson

---
+++ src/main/java/com/triceratops/triceratops/modele/ChaineProduction.java
@@ -0,0 +1,10 @@
+package com.triceratops.triceratops.modele;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChaineProduction {
+    private Map<Produit, Integer> produitIn = new HashMap<>();
+    private Produit produitOut;
+
+}
---
+++ src/main/java/com/triceratops/triceratops/modele/Produit.java
@@ -0,0 +1,11 @@
+package com.triceratops.triceratops.modele;
+
+public class Produit {
+    private int quantite;
+    private String code;
+    private String nom;
+    private float pAchat;
+    private float pVente;
+    private String unite; // Remplacer éventuellement par un enum
+
+}
